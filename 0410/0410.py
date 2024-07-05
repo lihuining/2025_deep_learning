bda6072c6c196da0cf98ad884cef5d60e6256804
“lihuining” <2427435319@qq.com>
2024-07-05 11:12:37 +0800

    Deleted several files

--- 0410/0410.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-for i in range(T):
-    n,x,k = list(map(int,input().split()))
-    ans = [0]*(n+1)
-    ans[-1] = x
-    for j in range(n-1,-1,-1):
-        ans[j] = ans[j+1] % j
-    print(ans[k-1])
-    
--- 0421/0421.py
+++
@@ -1,4 +0,0 @@
-MOD = 10**9 + 7
-n = int(input())
-num = n*(n-1) // 2 - (n-1) # 选取不相邻的对数
-print(num*25*pow(26,n-2,mod)%mod)
